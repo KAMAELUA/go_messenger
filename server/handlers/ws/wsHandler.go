6800df7651590d02fa7ad85d650be49916d816b8
Aleksjj <komarov.oleksii@gmail.com>
2018-06-04 19:13:50 +0300

    fixed json generating on client

--- server/handlers/ws/wsHandler.go
+++ server/handlers/ws/wsHandler.go
@@ -13,7 +13,7 @@ type Message struct {
 	UserName    string `json:"username"`
 	GroupName   string `json:"group_name"`
 	ContentType string `json:"content_type"`
-	Content     string `json:"message_content"`
+	Content     string `json:"content"`
 	Login       string `json:"login"`
 	Password    string `json:"-"`
 	Email       string `json:"email"`
